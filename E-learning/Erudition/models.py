087a98bac0f35996b7ad021497c062cf5efc0c26
Suhani kumari <suhanidhanupur@gmail.com>
2026-07-24 22:16:54 +0530

    Added

--- E-learning/Erudition/models.py
+++ E-learning/Erudition/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
